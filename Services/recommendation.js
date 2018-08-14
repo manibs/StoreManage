7d29e11b49275012d1a206647fe51e67e8146935
Mani <manibhushan.sharma@gmail.com>
2018-08-14 23:57:02 +0530

    commit

--- Services/recommendation.js
+++ Services/recommendation.js
@@ -5,11 +5,26 @@ var router = function (Recommendation) {
     var RecommendationRouter = express.router;
 
     RecommendationRouter.route('/recommendation')
-    .post(function(req, res){
-        var recommendation = new Recommendation(req.body);
-        recommendation.save();
-        res.status(201).send(recommendation);
-    })
+        .post(function (req, res) {
+            var recommendation = new Recommendation(req.body);
+            recommendation.save();
+            res.status(201).send(recommendation);
+        })
+        .get(function (req, res) {
+            var query = {};
+            
+            Product.find(query, function (err, recommendations) {
+
+                if (err) {
+                    res.status(500).send(err);
+                    console.log(err);
+                }
+                else {
+                    res.json(recommendations);
+                }
+            });
+
+        });
 };
 
 module.exports = router;
