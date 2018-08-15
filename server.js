6916c5fea7193d915d46006906f58ffd09bf173d
Mani <manibhushan.sharma@gmail.com>
2018-08-15 22:20:22 +0530

    commented new code

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ var orderRouter = require(app.get('src') +'Services/Orders')(Orders);
 app.use('/api', orderRouter);
 
 
-var Recommendation = require(app.get('src') + 'Models/recommendationModel');
-var RecommendationRouter = require(app.get('src') +'Services/Recommendation')(Recommendation);
+// var Recommendation = require(app.get('src') + 'Models/recommendationModel');
+// var RecommendationRouter = require(app.get('src') +'Services/Recommendation')(Recommendation);
 //app.use('/api', recommendationRouter);
 
 //CORS middleware
